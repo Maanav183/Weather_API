4b8e724b94a0fa2476d601e7227bde8ddf6b0fcb
Maaanav18 <maaanavbobade@gmail.com>
2025-11-27 23:24:44 +0530

    Prepare Vite React app for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Weather_API/',   // Add this line
+  base: '/Weather_API/',  // important!
   plugins: [react()],
 })
